f9a5178bc2b448d50b01789beed5a958e24e1aa4
dmaberlin1 <dmaberlin77@gmail.com>
2023-02-10 14:26:49 +0200

    feat: add context

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Categories from "./components/categories/Categories";
 import Sort from "./components/sort/Sort";
 import PizzaBlock from "./components/pizzaBlock/PizzaBlock";
 import pizzas from './assets/pizzas.json'
-import {useEffect, useState} from "react";
+import React,{useEffect, useState} from "react";
 import PizzaSkeleton from "./components/pizzaBlock/PizzaSkeleton";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import {Route, Routes} from "react-router-dom";
 import Cart from "./pages/Cart";
+
+export const SearchContext=React.createContext({})
 
 function App() {
     const [searchValue, setSearchValue] = useState('');
@@ -17,16 +19,16 @@ function App() {
 
     return (
       <div className="wrapper">
-          <Header searchValue={searchValue} setSearchValue={setSearchValue}></Header>
-          <div className="content">
+        <SearchContext.Provider value={{searchValue,setSearchValue}}>
+            <Header></Header>
+            <div className="content">
                 <Routes>
-                    <Route path={'/'} element={<Home searchValue={searchValue}/>}></Route>
+                    <Route path={'/'} element={<Home/>}></Route>
                     <Route path={'/cart'} element={<Cart/>}></Route>
                     <Route path={'*'} element={<NotFound/>}></Route>
-
                 </Routes>
-
-          </div>
+            </div>
+        </SearchContext.Provider>
       </div>
 
     );
